44b317c6ef5eff5475c3d0219dc2ad9e7a0a107b
Yuri <yuri01.sp@gmail.com>
2024-02-15 21:30:50 -0300

    Adicionando a pagina projetos

--- src/router/index.js
+++ src/router/index.js
@@ -9,17 +9,23 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+
     {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
     },
 
-
     {
       path: '/blog',
       name: 'blog',
       component: () => import('../views/BlogView.vue')
+    },
+
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('../views/ProjectsView.vue')
     }
   ]
 })
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div class="loading">
+      <h1>Página em construção</h1>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+@import '../scss/style.scss';
+
+.container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background-color: $cor-light;
+}
+
+
+.loading {
+  width: 70%;
+  max-width: 70%;
+}
+
+.loading h1{
+  display: inline;
+  margin: 0 auto;
+  font-size: 5rem;
+  color: $cor-textDark;
+}
+
+.loading span{
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: $cor-textDark;
+  margin: 0 auto;
+  animation: bounce .6s cubic-bezier(0.6, 0.1, 1, 0.4);
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+  margin-bottom: 10px;
+  margin-left: 2%;
+}
+
+
+.loading span:nth-child(1) {
+  animation-delay: .1s;
+}
+
+.loading span:nth-child(2) {
+  animation-delay: .2s;
+}
+
+.loading span:nth-child(3) {
+  animation-delay: .3s;
+}
+
+@keyframes bounce {
+  to{
+    transform: translateY(50px);
+  }
+
+}
+</style>
